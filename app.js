c65042211f68ec9157811467a85877f3c0e352b5
Noboru Ando <65776256+moshimoshipandasan@users.noreply.github.com>
2023-02-13 10:34:46 +0900

    u2

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const video = document.getElementById("video");
 const startButton = document.getElementById("start-button");
 const stopButton = document.getElementById("stop-button");
 const captureButton = document.getElementById("capture-button");
+const recordButton = document.getElementById("record-button");
+const downloadButton = document.getElementById("download-button");
 const canvas = document.getElementById("canvas");
 
 let stream;
+let mediaRecorder;
+let recordedChunks = [];
 
 startButton.addEventListener("click", async () => {
   try {
@@ -13,6 +17,7 @@ startButton.addEventListener("click", async () => {
     startButton.disabled = true;
     stopButton.disabled = false;
     captureButton.disabled = false;
+    recordButton.disabled = false;
   } catch (e) {
     console.error("カメラを起動できませんでした。", e);
   }
@@ -27,6 +32,8 @@ stopButton.addEventListener("click", () => {
     startButton.disabled = false;
     stopButton.disabled = true;
     captureButton.disabled = true;
+    recordButton.disabled = true;
+    recordedChunks = [];
   }
 });
 
@@ -35,11 +42,71 @@ captureButton.addEventListener("click", () => {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   context.drawImage(video, 0, 0, canvas.width, canvas.height);
-  const url = canvas.toDataURL();
+  canvas.toBlob((blob) => {
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "photo.png";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  }, "image/png");
+});
+
+recordButton.addEventListener("click", () => {
+  if (recordButton.textContent === "録画開始") {
+    startRecording();
+  } else {
+    stopRecording();
+    recordButton.textContent = "録画開始";
+    downloadButton.disabled = false;
+  }
+});
+
+function startRecording() {
+  recordedChunks = [];
+  const options = { mimeType: "video/webm" };
+  try {
+    mediaRecorder = new MediaRecorder(stream, options);
+  } catch (e) {
+    console.error("MediaRecorderを作成できませんでした。", e);
+    return;
+  }
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log("録画が開始されました。");
+  recordButton.textContent = "録画停止";
+  downloadButton.disabled = true;
+}
+
+function handleDataAvailable(event) {
+  if (event.data && event.data.size > 0) {
+    recordedChunks.push(event.data);
+  }
+}
+
+function stopRecording() {
+  mediaRecorder.stop();
+  console.log("録画が停止されました。Blobs:", recordedChunks);
+  download();
+}
+
+function download() {
+  const blob = new Blob(recordedChunks, { type: "video/webm" });
+  const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
   a.href = url;
-  a.download = "photo.png";
+  a.download = "video.mp4";
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+}
+
+window.addEventListener("beforeunload", () => {
+  if (stream) {
+    stream.getTracks().forEach((track) => {
+      track.stop();
+    });
+  }
 });
